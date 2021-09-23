800e882c4f3ff7fac50332690075a208f1f7add4
chaithrabhadran <chaithra080@gmail.com>
2021-09-24 00:05:46 +0530

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c8ad5b79-3022-4482-9e7d-c6daac8c49fd';
+export const MODAL_CLOSE_SEC = 2.5;
