242ea291fc3b9f05147cfd9b9ae0d089987fc429
chaithrabhadran <chaithra080@gmail.com>
2021-09-24 00:18:29 +0530

    new feature added

--- src/js/config.js
+++ src/js/config.js
@@ -3,3 +3,7 @@ export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = 'c8ad5b79-3022-4482-9e7d-c6daac8c49fd';
 export const MODAL_CLOSE_SEC = 2.5;
+const newf=function(){
+    console.log('hi');
+}
+newf();
